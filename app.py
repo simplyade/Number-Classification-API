d14cb3b52a5c2884f1c98a6fa163068d2071743d
simplyade <37074415+simplyade@users.noreply.github.com>
2025-02-07 14:53:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException,Query
 from number_classifier import classify_number, is_fun_fact
-from utils import get_fun_fact
-from pydantic import BaseModel
+
 
 app = FastAPI()
-
-class NumberRequest(BaseModel):
-    number: int
 
 @app.get("/")
 async def index():
     return {"message": "Welcome to the Number Classification API!"}
 
 @app.get("/api/classify-number/")
-async def api_classify_number(number_request: NumberRequest):
-    result = classify_number(number_request.number)
-    result['fun_fact'] = is_fun_fact(number_request.number)
-    result['external_fact'] = get_fun_fact(number_request.number)
+async def api_classify_number(number: int = Query(...)):
+    result = classify_number(number)
+    result['fun_fact'] = is_fun_fact(number)
     return result
+
+
 
 
 
